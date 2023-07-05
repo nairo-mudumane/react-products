ad1196d446f0c82ef8f7ff0fb8403d00cae270a7
nairo-mudumane <nairomudumane@outlook.com>
2023-07-05 16:25:37 +0200

    tailwind and mui instalation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Button, Paper } from "@mui/material";
+
 function App() {
-  return <div>React App</div>;
+  return (
+    <div>
+      <h1 className="font-bold underline mb-4">React App</h1>
+
+      <div className="flex gap-4">
+        <Button variant="contained">contained</Button>
+        <Button variant="outlined">outlined</Button>
+        <Button variant="text">text</Button>
+        <Paper>paper</Paper>
+      </div>
+    </div>
+  );
 }
 
 export default App;
